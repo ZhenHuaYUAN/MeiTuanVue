8c768dbed524ac4c61303416228b356f494cb57e
YuanZH <389937586@qq.com>
2019-03-28 16:47:34 +0800

    header‘

---
+++ components/public/header/topbar.vue
@@ -0,0 +1,32 @@
+<template>
+<!-- elementUI 的分栏布局 一共24个栅格  gutter 属性来指定每一栏之间的间隔，默认间隔为 0。-->
+    <el-row :gutter="0" class="m-header" style="color:#999">
+      <el-col :span="5" >
+        <geo></geo>
+      </el-col>
+      <el-col :span="5">
+        <user></user>
+      </el-col>
+      <el-col :span="14">
+        <navbar></navbar>
+      </el-col>
+    </el-row>
+</template>
+
+<script>
+import Geo from './geo.vue'
+import User from './user.vue'
+import Navbar from './nav.vue'
+export default {
+  components:{
+    Geo,
+    User,
+    Navbar
+  }
+};
+</script>
+
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss";
+  @import "@/assets/css/public/header/index.scss";
+</style>
---
+++ components/public/header/user.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="m-user">
+    <!-- 需要判断是否登录 -->
+    <template v-if="user">
+      欢迎您，
+      <span class="userName">{{ user }}</span>
+      [
+      <nuxt-link to="/exit">退出</nuxt-link>]
+    </template>
+    <template v-else>
+      <nuxt-link to="/login" class="login">立即登录</nuxt-link>
+      <nuxt-link class="register" to="/register" style="color:#999">注册</nuxt-link>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: ""
+    };
+  }
+};
+</script>
+
+<style>
+</style>
